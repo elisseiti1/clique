9e32088effac26135083e3ba868c4ae69de4ad5d
Elis Seiti <elisseiti95@gmail.com>
2021-11-15 00:05:05 +0100

    Methods to be implemented for clusters

--- Clique/__init__.py
+++ Clique/__init__.py
@@ -2,7 +2,6 @@ import numpy as np
 
 
 class Clique:
-
     # xi is the partition input parameter
     xi: int = 1
 
@@ -20,30 +19,89 @@ class Clique:
         self.tau = tau
         self.isPruning = pruning
 
-    def generate_oneDimensionalUnits(self, data, xsi, tau):
-        number_data_points = np.shape(data)[0]
-        number_features = np.shape(data)[1]
+    def fit(self, data):
+        self.data = data
 
-        subspaces = np.zeros(xsi, number_features)
+    def generate_oneDimensionalUnits(self):
+        number_data_points = np.shape(self.data)[0]
+        number_features = np.shape(self.data)[1]
 
-        # project nr
+        subspaces = np.zeros((self.xi, number_features))
+
+        # project data into each of the clusters
         for feature in range(number_features):
-            minVal = min(data[:, feature])
-            maxVal = max(data[:, feature])
-            interval = (maxVal - minVal)/xsi
-            for element in data[:, feature]:
-                index = (element - minVal)//interval
+            minVal = min(self.data[:, feature])
+            maxVal = max(self.data[:, feature])
+            interval = (maxVal - minVal) / self.xi
+            for element in self.data[:, feature]:
+                index = int((element - minVal) // interval)
+                if (element == maxVal):
+                    index = self.xi - 1
+
                 subspaces[index, feature] += 1
 
-        # print("1D projection:\n", subspaces, "\n")
-        is_dense = subspaces > tau * number_data_points
-        # print("is_dense:\n", is_dense)
+        print("1D projection:\n", subspaces, "\n")
+        is_dense = subspaces > self.tau * number_data_points
+        print("is_dense:\n", is_dense)
         one_dim_dense_units = []
-
         for f in range(number_features):
-            for unit in range(xsi):
-                if subspaces[unit, f] > tau:
-                    dense_unit = dict({f: unit})
+            for unit in range(self.xi):
+                if is_dense[unit, f]:
+                    dense_unit = dict()
+                    dense_unit[f] = unit
                     one_dim_dense_units.append(dense_unit)
         return one_dim_dense_units
 
+    def run_clique(self):
+        # Finding 1 dimensional dense units
+        dense_units = self.generate_oneDimensionalUnits()
+        number_features = np.shape(self.data)[1]
+        # Getting 1 dimensional clusters
+        # clusters = self.get_clusters(dense_units, self.data, self.xsi)
+        self.get_clusters(dense_units)
+        clusters = []
+        # Finding dense units and clusters for dimension > 2
+        current_dim = 2
+        number_of_features = np.shape(self.data)[1]
+
+        while (current_dim <= number_of_features) & (len(dense_units) > 0):
+            print("\n", str(current_dim), " dimensional clusters:")
+            dense_units = self.get_dense_units_for_dim(dense_units, current_dim)
+            for cluster in self.get_clusters(dense_units):
+                clusters.append(cluster)
+            current_dim += 1
+
+        return clusters
+
+    # def get_clusters(self, dense_units):
+    #     graph = build_graph_from_dense_units(dense_units)
+    #     number_of_components, component_list = scipy.sparse.csgraph.connected_components(
+    #         graph, directed=False)
+    #
+    #     dense_units = np.array(dense_units)
+    #     clusters = []
+    #     # For every cluster
+    #     for i in range(number_of_components):
+    #         # Get dense units of the cluster
+    #         cluster_dense_units = dense_units[np.where(component_list == i)]
+    #         print("cluster_dense_units: ", cluster_dense_units.tolist())
+    #
+    #         # Get dimensions of the cluster
+    #         dimensions = set()
+    #         for u in cluster_dense_units:
+    #             dimensions.update(u.keys())
+    #
+    #         # Get points of the cluster
+    #         cluster_data_point_ids = get_cluster_data_point_ids(
+    #             data, cluster_dense_units)
+    #         # Add cluster to list
+    #         clusters.append(Cluster(cluster_dense_units,
+    #                                 dimensions, cluster_data_point_ids))
+
+        return clusters
+
+    def get_dense_units_for_dim(data, dense_units, current_dim):
+        pass
+
+    def get_clusters(self, dense_units):
+        pass
--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+from Clique import Clique
+import numpy as np
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -11,6 +12,17 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    a = np.array([
+        [0.45660137634625386, 0.43280640922410835],
+        [0.6113784672224188, 0.5286245988894975],
+        [0.45029897412145387, 0.7116061205092745],
+        [0.6390150501606866, 0.46074398219372076],
+        [0.6289567839292338, 0.32346951478531516]
+        ])
+    number_data_points = np.shape(a)[0]
 
+    clique = Clique(xi=3, tau=0.2, pruning=False)
+    clique.fit(data=a)
+    # print(clique.generate_oneDimensionalUnits())
+    clique.run_clique()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
